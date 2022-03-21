24d4cf12f76739632b0270a7a711f261e29cb030
Kamelfluesterer <silvanvanveen@gmail.com>
2022-03-21 21:08:06 +0100

    add edit function, add scrollToTop function and Btn

--- react-app/src/components/ChallengeComponents/Challenges.js
+++ react-app/src/components/ChallengeComponents/Challenges.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-// import PlusIcon from '../IconComponents/PlusIcon.js';
 import Header from '../LayoutComponents/Header';
 import PlusIcon from '../IconComponents/PlusIcon';
 import CreateChallenge from './CreateChallenge';
 import Card from './Card';
+import ScrollTopBtn from './ScrollTopBtn';
+import { useNavigate } from 'react-router-dom';
 
 // import XHR from '../../XHR'; // Read documentation about auhtorization header problem
 
@@ -11,17 +12,15 @@ const Challenges = (props) => {
     // const {} = props;
 
     const [ create, setCreate ] = useState(false);
+    const [ edit, setEdit ] = useState({});
+    const [ topBtn, setTopBtn ] = useState(false);
     const [ sort, setSort ] = useState( 'id' );
     const [ filter, setFilter ] = useState( 'getCommunity' );
     const [ data, setData ] = useState( [] );
-
+    const [ scrollToTop, setScrollToTop ] = useState(false);
+    const navigate = useNavigate();
 
     useEffect( () => {
-
-        // read documentation about authorization header problem:
-        // let token = localStorage.getItem( 'jwt' );
-
-        // console.log(token);
 
         fetch( `http://localhost:8888/api/challenges/${filter}/${sort}`, {
             // headers: {
@@ -31,35 +30,71 @@ const Challenges = (props) => {
         })
             .then(res => res.json())
             .then((data) => {
-                console.log(data);
+                /* console.log(data); */
                 const dataArr = Object.values(data.result);
 
                 setData(dataArr);
             })
             .catch(err=> console.log(err));
         
-    }, [ sort, filter] );
+    }, [ sort, filter ] );
+
+    useEffect ( () => {
+
+        if (scrollToTop) {
+            document.querySelector('.challenges-wrapper').scrollTo(0, 0);
+            setScrollToTop(false);
+        };
+
+    }, [ scrollToTop ])
 
 
     function display(e) {
         e.currentTarget.children[1].classList.toggle('display-dropdown');
     }
+
+    function handleTopBtn(e) {
+        if ( e.target.scrollTop === 0 ) {
+            setTopBtn(false);
+        } else { 
+            setTopBtn(true);
+        }
+    }
+
+    function logout() {
+        //logout
+        fetch( 'http://localhost:8888/api/user/logout', {
+            method: 'PUT'
+        })
+        .then( (res) => res.json() )
+        .then( (data) => console.log(data))
+        .catch( (err) => console.log(err))
+
+        if ( data.success ) {
+            navigate('/start');
+        }        
+    }
      
     return (
         <>
-            <div className="challenges-wrapper">
+            <div className="challenges-wrapper" onScroll={(e) => handleTopBtn(e)}>
                 
                 <Header>
                     <h1>Challenges</h1>
                     {
-                        (!create) ? 
+                        ( !create && Object.keys(edit).length === 0 ) ? 
                             <>
-                                <div className="header-button" onClick={() => {setCreate(true); document.querySelector('body').style.overflowY = 'hidden'}}>
-                                    <PlusIcon/>
+                                <div className="header-button--wrapper">
+                                    <div className="header-button" onClick={() => {setCreate(true); document.querySelector('body').style.overflowY = 'hidden'}}>
+                                        <PlusIcon />
+                                    </div>
+                                    <div className="header-button header-button--create" onClick={() => logout()}>
+                                        <i className="fas fa-sign-out-alt" title="Logout"></i>
+                                    </div>
                                 </div>
                             </> : 
                             <>
-                                <div className="header-button" onClick={() => {setCreate(false); document.querySelector('body').style.overflowY = 'hidden'}}>
+                                <div className="header-button" onClick={() => {setCreate(false); setEdit({}); document.querySelector('body').style.overflowY = 'hidden'}}>
                                     <span className='fas fa-times button-close'></span>
                                 </div>
                             </>
@@ -68,8 +103,10 @@ const Challenges = (props) => {
                 <main>
                     <div className="content-view">
 
-                        {create ? <CreateChallenge></CreateChallenge> : <></>}
-                    
+                        {create ? <CreateChallenge mode="create"></CreateChallenge> : <></>}
+                        
+                        {(Object.keys(edit).length !== 0) ? <CreateChallenge mode="edit" values={edit}></CreateChallenge> : <></>}
+
                         <div className="filterbar mb-2">
                             <div className="filter--sort-wrapper me-1" onClick={(e) => display(e)}>
                                 <div className="btn filter-btn filter--sort">
@@ -79,7 +116,7 @@ const Challenges = (props) => {
                                 <div className="sort-dropdown">
                                     <span className="sort-option" onClick={() => setSort('title')}>Nach Titel sortieren</span>
                                     <span className="sort-option" onClick={() => setSort('level')}>Nach Level sortieren</span>
-                                    <span className="sort-option" onClick={() => setSort('username')}>Nach Usernamen</span>
+                                    <span className="sort-option" onClick={() => setSort('username')}>Nach Usernamen sortieren</span>
                                     <span className="sort-option" onClick={() => setSort('id')}>Älteste zuerst</span>
                                 </div>
                             </div>
@@ -94,24 +131,36 @@ const Challenges = (props) => {
                         </div>
 
                         <div className="cards-wrapper">
+                            
                             {
-                                data.map( (challenge, key ) => {
+                                data.map( ( challenge ) => {
+                                    
                                     return ( 
+
                                         <Card 
-                                            key={key} 
+                                            key={challenge.id}
+                                            cid={challenge.id}
+                                            setEdit={setEdit} 
                                             filter={filter} 
                                             title={challenge.title}
                                             description={challenge.description}
                                             username={challenge.username}
                                             filename={challenge.filename}
                                             pokemon={challenge.name}
+                                            question={challenge.content}
                                             level={challenge.level}
-                                            reward={challenge.question_level}                                            
+                                            reward={challenge.question_level}                                         
                                         >
                                         </Card>
                                 )} )
                             }
                         </div>
+                        
+                        {
+                            (topBtn) ?
+                            <ScrollTopBtn setScrollToTop={setScrollToTop}></ScrollTopBtn> : <></>
+                        }
+                        
 
                     </div>
                 </main>
