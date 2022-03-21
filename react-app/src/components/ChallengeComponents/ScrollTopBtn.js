d9537f0fb25231900d99b168c1fbc99f1f1206c7
Kamelfluesterer <silvanvanveen@gmail.com>
2022-03-21 21:15:24 +0100

    add scrollToTopBtn

---
+++ react-app/src/components/ChallengeComponents/ScrollTopBtn.js
@@ -0,0 +1,14 @@
+
+const ScrollTopBtn = ({setScrollToTop}) => {
+
+    return (
+
+        <>
+            <div className="scrollTopBtn">
+                <i className="fas fa-arrow-alt-circle-up" onClick={() => setScrollToTop(true)}></i>
+            </div>
+        </>
+    )
+}
+
+export default ScrollTopBtn;
