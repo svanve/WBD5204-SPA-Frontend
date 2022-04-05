68759046e0af00cf64d9c42281090a02505a1737
Kamelfluesterer <silvanvanveen@gmail.com>
2022-04-06 00:30:02 +0200

    nothing really

--- react-app/src/components/ChallengeComponents/WinStateModal.js
+++ react-app/src/components/ChallengeComponents/WinStateModal.js
@@ -2,21 +2,6 @@ import React from 'react';
 
 const WinStateModal = (props) => {
 
-    // Add-on: read documentation for further info
-    // if (!props.winState[0]) {levelUp(props.pokemon_id)};
-    // function levelUp (pokemon_id) {
-    //     // const token = localStorage.getItem( 'jwt' ); 
-        
-    //     fetch( `${process.env.REACT_APP_BACKEND_URI}/api/challenges/delete/${challenge_id}`, {
-    //         method: 'PUT',
-    //         // headers: {
-    //         //     'Content-Type': 'application/json',
-    //         //     'credentials': 'include',
-    //         //     'authorization': token
-    //         // }
-    //     })
-    // }
-
     return (
         <>
             <div className="modal" tabIndex="-1" style={{display: 'block'}}>
@@ -33,7 +18,7 @@ const WinStateModal = (props) => {
                     </div>
                     <div className="modal-footer">
                         <button className="win-state-modal--btn btn btn-primary" onClick={() => {props.setWinModal(false); props.setGameMode(false)}}>
-                            <i class="fas fa-undo-alt me-2"></i>
+                            <i className="fas fa-undo-alt me-2"></i>
                             <span className="delete-span">Zu den Challenges</span>
                         </button>
                     </div>
