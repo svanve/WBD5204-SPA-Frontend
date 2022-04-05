ebb5f55ed0376170a1d697a0bc04474b3f36b598
Kamelfluesterer <silvanvanveen@gmail.com>
2022-04-06 00:27:43 +0200

    delete height and width

--- react-app/src/components/iconComponents/AshIcon.js
+++ react-app/src/components/iconComponents/AshIcon.js
@@ -6,8 +6,6 @@ const AshIcon = (props) => {
     <>
         <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="444"
-        height="486"
         viewBox="0 0 444 486"
         >
             <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
