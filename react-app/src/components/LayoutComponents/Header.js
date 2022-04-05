ff4d700f9dab5129d58a4d2c4e54e675f32ef794
Kamelfluesterer <silvanvanveen@gmail.com>
2022-04-06 00:20:49 +0200

    header resueable

--- react-app/src/components/LayoutComponents/Header.js
+++ react-app/src/components/LayoutComponents/Header.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Menubar from './Menubar';
+import PlusIcon from '../IconComponents/PlusIcon';
 
-const Header = ( props ) => {
+import { Context } from '../../helpers/Context';
+import { logout } from '../../helpers/Helpers';
+
+const Header = ({pageTitle}) => {
+
+    const { create, setCreate, edit, setEdit } = useContext(Context);
 
     return (
     <>
         <header className="header">
             
             <div className="headerbar">
-                {props.children}
+                <h1>{pageTitle}</h1>
+                        {
+                            ( !create && Object.keys(edit).length === 0 ) ? 
+                                <>
+                                    <div className="header-button--wrapper">
+                                        <div className="header-button" onClick={() => {setCreate(true); document.querySelector('body').style.overflowY = 'hidden'}}>
+                                            <PlusIcon />
+                                        </div>
+                                        <div className="header-button header-button--create" onClick={() => logout()}>
+                                            <i className="fas fa-sign-out-alt" title="Logout"></i>
+                                        </div>
+                                    </div>
+                                </> : 
+                                <>
+                                    <div className="header-button" onClick={() => {setCreate(false); setEdit({}); document.querySelector('body').style.overflowY = 'hidden'}}>
+                                        <span className='fas fa-times button-close'></span>
+                                    </div>
+                                </>
+                        }
             </div>
 
             <Menubar></Menubar>
